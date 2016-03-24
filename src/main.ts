a40521adb94eafc08d8bf3bed7c4c5f13dd1bff6
Fernando Cortez <fccortez1@hotmail.com>
2016-03-23 21:36:17 -0400

    Switched from Skeleton CSS to Material Design Lite. Renamed app component to follow current Angular2 style guide.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './AppComponent.ts';
+import {AppComponent} from './app.component.ts';
 
 bootstrap(AppComponent);
